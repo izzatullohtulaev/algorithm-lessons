633875407da561340701485b2903fea8e2665a48
Izzat Tulayev <izzatullohman@gmail.com>
2023-07-25 00:28:55 +0500

    zor

---
+++ np-problems/traveller.py
@@ -0,0 +1,29 @@
+cities = {
+    'ab':5,
+    'ac':9,
+    'ad':13,
+    'ae':12,
+    'ba':6,
+    'bc':7,
+    'bd':10,
+    'be':6,
+    'ca':12,
+    'cb':7,
+    'cd':3,
+    'ce':15,
+    'da':8,
+    'db':20,
+    'dc':8,
+    'de':2,
+    'ea':13,
+    'eb':5,
+    'ec':9,
+    'ed':3,
+    }
+searched = set()
+# while cities:
+#     city = 
+min_dis = min(cities)
+# city = cities.pop()
+
+kalitlar = cities.keys()
